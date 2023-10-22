9f55e7412407e311e71999399c6f2ddc757fb873
Fajar Dwi Utomo <fajardwiutomo75@gmail.com>
2023-10-22 13:38:48 +0700

    Update user.controller.js

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,23 +1,23 @@
-const { ResponseTemplate } = require('')
+const { ResponseTemplate } = require('../helper/template.helper')
 
-function TestUser(req, res){
-    res.json({
-        "data":null,
-        "message":"Berhasil Masuk User",
-        "status":200
-       })
+function TestUser(req, res) {
+    let resp = ResponseTemplate(null, 'success', null, 200)
+    res.json(resp)
 }
 
-function TestUserPost(req, res){
-    res.json({
-        "data":null,
-        "message":"Berhasil Masuk User",
-        "status":200
-       })
+function TestUserPost(req, res) {
+    console.log(req.query.name)
+    let objResp = {
+        name: req.body.name,
+        address: req.body.address
+    }
+    let resp = ResponseTemplate(objResp, 'success', null, 200)
+    res.json(resp)
 }
 
 
 
-module.exports ={
+module.exports = {
     TestUser,
+    TestUserPost
 }
