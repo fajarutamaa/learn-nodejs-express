35cb64fb69918dfc48621ed1a3ac00e158022f59
Fajar Dwi Utomo <fajardwiutomo75@gmail.com>
2023-10-25 18:21:12 +0700

    add create, read, and delete method

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,24 +1,63 @@
 const { ResponseTemplate } = require('../helper/template.helper')
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
 
 function TestUser(req, res) {
     let resp = ResponseTemplate(null, 'success', null, 200)
     res.json(resp)
 }
 
-function TestUserPost(req, res) {
-    const { name, address } = req.body
-    try {
-        // if(!name || !address){
-        //     let resp = ResponseTemplate(null, 'bad request', new Error('bad request'), 400)
-        //     res.json(resp)
-        //     return
-        // }
+async function Insert(req, res) {
+    const { name, email, address } = req.body
+    const payload = {
+        name,
+        address,
+        email
+    }
 
-        let objResp = {
-            name: req.body.name,
-            address: req.body.address
-        }
-        let resp = ResponseTemplate(objResp, 'success', null, 200)
+    try {
+
+        const user = await prisma.user.create({
+            data: payload
+        })
+
+        let resp = ResponseTemplate(user, 'success', null, 200)
+        res.json(resp)
+        return
+    } catch (error) {
+        let resp = ResponseTemplate(null, 'internal server error', error, 500)
+        res.json(resp)
+        return
+    }
+}
+
+async function Get(req, res) {
+
+    const { name, email } = req.query
+    const payload = {}
+
+    if (name) {
+        payload.name = name
+    }
+
+    if (email) {
+        payload.email = email
+    }
+
+    try {
+        const users = await prisma.user.findMany({
+            where: payload,
+            include: {
+                post: {
+                    where: {
+                        title: 'Pemilu Seru Habis'
+                    }
+                }
+            }
+
+        })
+        let resp = ResponseTemplate(users, 'success', null, 200)
         res.json(resp)
         return
     } catch (error) {
@@ -29,9 +68,98 @@ function TestUserPost(req, res) {
 
 }
 
+async function GetByPK(req, res) {
+
+    const { id } = req.params
+
+    try {
+        const users = await prisma.user.findUnique({
+            where: {
+                id: Number(id)
+            },
+        })
+        let resp = ResponseTemplate(users, 'success', null, 200)
+        res.json(resp)
+        return
+    } catch (error) {
+        let resp = ResponseTemplate(null, 'internal server error', error, 500)
+        res.json(resp)
+        return
+    }
+}
+
+async function Update(req, res) {
+
+    const { name, email, address } = req.body
+    const { id } = req.params
+
+    const payload = {}
+
+    if (!name && !email && !address) {
+        let resp = ResponseTemplate(null, 'bad request', null, 400)
+        res.json(resp)
+        return
+    }
+
+    if (name) {
+        payload.name = name
+    }
+
+    if (email) {
+        payload.email = email
+    }
+
+    if (address) {
+        payload.address = address
+    }
+
+    try {
+        const user = await prisma.user.update({
+            where: {
+                id: Number(id)
+            },
+            data: payload
+        })
+
+        let resp = ResponseTemplate(user, 'success', null, 200)
+        res.json(resp)
+        return
+    }
+    catch (error) {
+        let resp = ResponseTemplate(null, 'internal server error', null, 500)
+        res.json(resp)
+        return
+    }
+}
+
+async function Delete(res, req) {
+
+    const { id } = req.params
+
+    try {
+        const user = await prisma.user.delete({
+            where: {
+                id: Number(id)
+            }
+        })
+
+        let resp = ResponseTemplate(null, 'success', null, 200)
+        res.json(resp)
+        return
+    } catch (error) {
+        let resp = ResponseTemplate(null, 'internal server error', null, 500)
+        res.json(resp)
+        return
+    }
+}
+
 
 
 module.exports = {
     TestUser,
-    TestUserPost
+    Insert,
+    Get,
+    GetByPK,
+    Update,
+    Delete
 }
