96e970d00113b9d5256fe0ada0b5665af7c127c2
Fajar Dwi Utomo <fajardwiutomo75@gmail.com>
2023-10-25 18:31:41 +0700

    update validation check post and request

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,29 +1,36 @@
 const { ResponseTemplate } = require('../helper/template.helper')
+const Joi = require('joi')
 
-function PrintSuccess(req, res, next) {
-    console.log(` Selalu Berhasil Akses `)
-    next()
-}
+// function PrintSuccess(req, res, next) {
+//     const { } = req.params.id
+//     console.log(` Selalu Berhasil Akses `)
+//     next()
+// }
 
 
-function PrintSuccessRoute(req, res, next) {
-    console.log(` Selalu Berhasil Akses Melalui Route Level`)
-    next()
-}
+// function PrintSuccessRoute(req, res, next) {
+//     console.log(` Selalu Berhasil Akses Melalui Route Level`)
+//     next()
+// }
 
 function CheckPostReq(req, res, next) {
+    const schema = Joi.object({
+        name: Joi.string().aphanum().max(255).required(),
+        address: Joi.string().alphanum().required(),
+        email: Joi.string().alphanum().email().required()
+    })
 
-    if (req.body.name == undefined || req.body.addres == undefined) {
-        let respErr = ResponseTemplate(null, 'invalid request',
-            new Error('invalid request', 400))
-        res.json(respErr)
+    const { error } = schema.validate(req.body)
+    if (error) {
+        let respError = ResponseTemplate(null, 'invalid request', error.details[0].message, 400)
+        res.json(respError)
+        return
     }
 
     next()
 }
 
+
 module.exports = {
-    PrintSuccess,
-    PrintSuccessRoute,
     CheckPostReq
 }
