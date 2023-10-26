2bce8e8b8234c91d1ae55aafafc96f6082836961
fajarutamaa <fajardwiutomo75@gmail.com>
2023-10-26 17:22:24 +0700

    add ORM prisma

---
+++ prisma/migrations/20231026102035_update_table/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `authorId` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `content` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `published` on the `Post` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[name]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `author_id` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `address` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "authorId",
+DROP COLUMN "content",
+DROP COLUMN "published",
+ADD COLUMN     "author_id" INTEGER NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "address" TEXT NOT NULL,
+ALTER COLUMN "name" SET NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_name_key" ON "User"("name");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
